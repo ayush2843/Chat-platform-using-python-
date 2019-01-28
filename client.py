6dad2adf4d5252a2907de34ddec51df6feb5edae
ayush2843 <ayush.agarwal@iitgn.ac.in>
2019-01-28 20:04:35 +0530

    port change

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    port = 6000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
@@ -11,7 +11,8 @@ def client_program():
     message = input(" -> ")  # take input
 
     while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
+        client_socket.send(message.encode())  # send m
+        essage
         data = client_socket.recv(1024).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+    port = 6000  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
